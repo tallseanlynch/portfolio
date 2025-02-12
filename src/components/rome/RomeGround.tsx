731a7d41f8ef2ace653190eec26338f6dfecfb54
Sean Lynch <tallseanlynch@gmail.com>
2025-02-11 18:00:12 -0600

    Added reflection plane into rome

--- src/components/rome/RomeGround.tsx
+++ src/components/rome/RomeGround.tsx
@@ -1,6 +1,6 @@
-import { TextureLoader } from 'three';
-import { useLoader } from '@react-three/fiber';
-import { whiteColor } from './romeColors';
+import { MeshReflectorMaterial } from '@react-three/drei';
+import { TextureLoader } from 'three'; 
+import { useLoader } from "@react-three/fiber";
 
 const RomeGround: React.FC = (): JSX.Element => {
     const groundTexture = useLoader(TextureLoader, '/rome/ground-inverse-compressed.jpg')
@@ -9,16 +9,23 @@ const RomeGround: React.FC = (): JSX.Element => {
         <mesh
             position={[0, 0, 0]}
             rotation={[-Math.PI * .5, 0, 0]}
-            receiveShadow
         >
-            <planeGeometry
+            <planeGeometry 
                 args={[5, 5, 1, 1]}
             />
-            <meshStandardMaterial
-                color={whiteColor}
-                map={groundTexture}
-                transparent={true}
-                alphaTest={0.5}
+            <MeshReflectorMaterial
+                blur={[0, 0]}
+                mixBlur={0}
+                mixStrength={10}
+                mixContrast={1}
+                resolution={256}
+                mirror={1}
+                depthScale={0}
+                minDepthThreshold={0.9}
+                maxDepthThreshold={1}
+                depthToBlurRatioBias={0.25}
+                distortion={1}
+                distortionMap={groundTexture}
             />
         </mesh>
     )
