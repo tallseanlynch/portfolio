45fa3aed2c98495759e3f6dd72d3fa90e643e492
Sean Lynch <tallseanlynch@gmail.com>
2025-04-02 15:15:26 -0500

    2nd building complete.

--- src/components/walking/WalkingBuildings.tsx
+++ src/components/walking/WalkingBuildings.tsx
@@ -71,6 +71,116 @@ const building0Shader = {
     }
 } 
 
+const building1ColorA = new Color(0x555555);
+const building1ColorB = new Color(0xededed);
+
+const building1Shader = {
+    vertexShader: `
+        varying vec2 vUv;
+        varying vec3 vPosition;
+
+        void main() {
+            vUv = uv;
+            vPosition = position;
+            gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+        }
+    `,
+    fragmentShader: `
+        uniform vec3 uColorA;
+        uniform vec3 uColorB;
+        uniform vec3 uGlassColor;
+        varying vec2 vUv;
+        varying vec3 vPosition;
+
+        void main() {
+            vec4 whiteColor = vec4(1.0, 1.0, 1.0, 1.0);
+            vec4 blackColor = vec4(0.0, 0.0, 0.0, 1.0);
+            vec4 transparentColor = vec4(0.0, 0.0, 0.0, 0.0);
+            vec4 baseColorA = vec4(uColorA, 1.0);
+            vec4 baseColorB = vec4(uColorB, 1.0);
+            vec4 baseGlassColor = vec4(uGlassColor, 1.0);
+            vec4 darkGlassColor = mix(blackColor, baseGlassColor, .5);
+            vec4 lighterGlassColor = mix(baseGlassColor, whiteColor, .5);
+            vec4 finalColor = mix(baseColorA, baseColorB, vPosition.y / 20.0);
+            // finalColor = vec4(vUv.x, vUv.y, 0.0, 1.0);
+            float vUvXFract = fract(vUv.x * 10.0);
+
+            if(vUvXFract > .1 && vUvXFract < .9) {
+                finalColor = mix(baseColorA, baseGlassColor, vUv.y);
+            } 
+
+            if(vUvXFract > .48 && vUvXFract < .52) {
+                finalColor = whiteColor;
+            } 
+
+            // bottom brick
+
+            // if(vPosition.y < -12.0 && mod(vUvXFract, .1) > 0.01 && mod(vUvXFract, .1) < 0.09) {
+            //     finalColor = mix(blackColor, baseColorB, .5);
+            //     // finalColor = whiteColor;
+            // }
+
+            // horizontal stripes
+
+            if(vPosition.y < -12.0 && vUv.x > 0.4 && vUv.x < 0.6) {
+                finalColor = baseColorA;
+            }
+
+            if(vPosition.y < -12.0 && vUv.x > 0.4 && vUv.x < 0.42) {
+                finalColor = whiteColor;
+            }
+
+            if(vPosition.y < -12.0 && vUv.x > 0.58 && vUv.x < 0.6) {
+                finalColor = whiteColor;
+            }
+
+            if(vPosition.y > 12.0 && vPosition.y < 12.5) {
+                finalColor = whiteColor;
+            }
+
+            // bottom side doors
+
+            if(vPosition.y > 6.0 && vPosition.y < 6.5) {
+                finalColor = whiteColor;
+            }
+
+            if(vPosition.y > 0.0 && vPosition.y < 0.5) {
+                finalColor = whiteColor;
+            }
+
+            if(vPosition.y > -12.0 && vPosition.y < -11.5) {
+                finalColor = whiteColor;
+            }
+
+            // roof
+
+            if(vPosition.y > 18.0) {
+                finalColor = baseColorB;
+            }
+
+            if(vPosition.y > 19.5) {
+                finalColor = mix(baseColorA, baseColorB, .2);
+            }
+
+            if(vPosition.y > 22.5) {
+                finalColor = blackColor;
+            }
+
+            if(vPosition.y < -6.0 && vPosition.y > -6.5) {
+                finalColor = whiteColor;
+            }
+
+            gl_FragColor = finalColor;
+        }
+    `,
+    uniforms: {
+        uColorA: { value: new Color(0x005f73) },
+        uColorB: { value: building1ColorB },
+        uGlassColor: { value: new Color(0x4f772d)}
+    }
+} 
+
+
 const WalkingBuildings = () => {
     return (
         <group>
@@ -86,9 +196,10 @@ const WalkingBuildings = () => {
                 <meshBasicMaterial color={0x00ff00} />
             </mesh>
             {/* top right */}
-            <mesh position={[92.5, 20, -92.5]}>
-                <boxGeometry args={[115, 40, 115]} />
-                <meshBasicMaterial color={0x0000ff} />
+            <mesh position={[122.5, 25, -92.5]}>
+                <boxGeometry args={[40, 50, 115]} />
+                <shaderMaterial {...building1Shader} />
+                {/* <meshBasicMaterial color={0x0000ff} /> */}
             </mesh>
             {/* top left */}
             <mesh position={[-95.5, 20, -92.5]}>
